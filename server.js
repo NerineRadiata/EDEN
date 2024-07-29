32b5edb04905591b37a087e5b8bd561f4ce80dff
NerineRadiata <129670885+NerineRadiata@users.noreply.github.com>
2024-07-29 22:29:55 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 
 // Thiết lập AWS SDK để sử dụng với Cloudflare R2
 const s3 = new AWS.S3({
-  endpoint: 'https://a24ed95912e33d89079396931e498bbf.r2.cloudflarestorage.com/images',
+  endpoint: 'https://a24ed95912e33d89079396931e498bbf.r2.cloudflarestorage.com',
   accessKeyId: process.env.R2_ACCESS_KEY_ID,
   secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
   s3ForcePathStyle: true,
@@ -18,12 +18,12 @@ const s3 = new AWS.S3({
 app.get('/api/images', async (req, res) => {
   try {
     const data = await s3.listObjectsV2({
-      Bucket: 'my-image-bucket',
+      Bucket: 'images', // Tên bucket của bạn
       Prefix: 'images/', // Thư mục chứa ảnh
     }).promise();
 
     const images = data.Contents.map(item => ({
-      file: `https://dash.cloudflare.com/a24ed95912e33d89079396931e498bbf/r2/default/buckets/images/${item.Key}`,
+      file: `https://a24ed95912e33d89079396931e498bbf.r2.cloudflarestorage.com/${item.Key}`,
       title: item.Key.split('/').pop().split('.')[0], // Sử dụng tên file làm title
     }));
 
